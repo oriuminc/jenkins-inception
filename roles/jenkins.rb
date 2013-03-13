e7a1b9999afb2ec7575a27cd8f6c324bcf14c1fe
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-12 21:42:59 -0400

    Checkstyle now working and warns for any errors, but still kicks off next job.

--- roles/jenkins.rb
+++ roles/jenkins.rb
@@ -37,6 +37,7 @@ default_attributes({
     "server" => {
       "host" => "0.0.0.0",
       "plugins" => [
+        "analysis-core",
         "ansicolor",
         "build-pipeline-plugin",
         "checkstyle",
