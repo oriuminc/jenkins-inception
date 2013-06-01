1033206810626530fd7163d480f750873ee27e1e
Patrick Connolly <patrick@myplanetdigital.com>
2013-05-31 21:54:58 -0400

    Symlinks ruby bins into place.

--- cookbooks-override/inception/recipes/compass.rb
+++ cookbooks-override/inception/recipes/compass.rb
@@ -1,3 +1,12 @@
 package "rubygems1.9.1"
 
+%w( ruby irb erb testrb rdoc gem rake ).each do |name|
+  path = ::File.join('/usr/bin', name)
+
+  link path do
+    to path + "1.9.1"
+    action :create
+  end
+end
+
 gem_package "compass"
