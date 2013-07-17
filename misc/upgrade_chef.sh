8fa569bc896d28716ede29d1ccb9982909e2e7d0
Patrick Connolly <patrick@myplanetdigital.com>
2013-07-17 17:25:04 -0400

    Removes misc dir as no longer need scripts.

--- misc/upgrade_chef.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "`knife -v | awk '{print $NF}'`" != "$1" ]; then
-  echo "Upgrading Chef to $1...";
-  gem uninstall chef --all --ignore-dependencies --executables
-  gem install chef -v $1 --no-rdoc --no-ri
-fi
